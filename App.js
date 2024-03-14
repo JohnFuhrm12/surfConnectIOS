69e05cc977935190aede4297c2577dba3f83a341
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-03-13 23:58:07 -0300

    Added bottom navbar

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>surfConnect</Text>
+      <View style={styles.navbar}>
+        <Text>Home</Text>
+        <Text>Explorer</Text>
+        <Text>Notifications</Text>
+        <Text>Profile</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +23,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 20,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom: 0,
+    borderTopWidth: 1,
+    borderTopColor: 'blue',
+    alignSelf: 'stretch',
+    width:'100%',
+    paddingTop: 30,
+    paddingBottom: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+    },
 });
